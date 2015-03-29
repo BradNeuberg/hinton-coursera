964474cfa9ef68f43415d96dae885c42115870e8
Brad Neuberg <brad@inkling.com>
2015-03-29 12:24:27 -0700

    Initial unanswered assignment 4.

---
+++ assignment4/cd1.m
@@ -0,0 +1,6 @@
+function ret = cd1(rbm_w, visible_data)
+% <rbm_w> is a matrix of size <number of hidden units> by <number of visible units>
+% <visible_data> is a (possibly but not necessarily binary) matrix of size <number of visible units> by <number of data cases>
+% The returned value is the gradient approximation produced by CD-1. It's of the same shape as <rbm_w>.
+    error('not yet implemented');
+end
