c646db3c02da8c77542d159cc2c371d4f8dae9b3
Brad Neuberg <brad@inkling.com>
2015-03-30 10:11:10 -0700

    Partial answers to PA4 - still filling out rest of problem.

--- assignment4/configuration_goodness.m
+++ assignment4/configuration_goodness.m
@@ -1,7 +1,20 @@
 function G = configuration_goodness(rbm_w, visible_state, hidden_state)
-% <rbm_w> is a matrix of size <number of hidden units> by <number of visible units>
-% <visible_state> is a binary matrix of size <number of visible units> by <number of configurations that we're handling in parallel>.
-% <hidden_state> is a binary matrix of size <number of hidden units> by <number of configurations that we're handling in parallel>.
-% This returns a scalar: the mean over cases of the goodness (negative energy) of the described configurations.
-    error('not yet implemented');
+  % <rbm_w> is a matrix of size <number of hidden units> by <number of visible units>
+  % <visible_state> is a binary matrix of size <number of visible units> by <number of configurations that we're handling in parallel>.
+  % <hidden_state> is a binary matrix of size <number of hidden units> by <number of configurations that we're handling in parallel>.
+  % This returns a scalar: the mean over cases of the goodness (negative energy) of the described configurations.
+  m = size(hidden_state, 2);
+  number_hidden = size(rbm_w, 1);
+  number_visible = size(rbm_w, 2);
+  G = 0;
+
+  for k = 1:m
+    for i = 1:number_visible
+      for j = 1:number_hidden
+        G += visible_state(i, k) .* rbm_w(j, i) .* hidden_state(j, k);
+      end
+    end
+  end
+
+  G = G / m;
 end
--- assignment4/visible_state_to_hidden_probabilities.m
+++ assignment4/visible_state_to_hidden_probabilities.m
@@ -1,7 +1,15 @@
 function hidden_probability = visible_state_to_hidden_probabilities(rbm_w, visible_state)
-% <rbm_w> is a matrix of size <number of hidden units> by <number of visible units>
-% <visible_state> is a binary matrix of size <number of visible units> by <number of configurations that we're handling in parallel>.
-% The returned value is a matrix of size <number of hidden units> by <number of configurations that we're handling in parallel>.
-% This takes in the (binary) states of the visible units, and returns the activation probabilities of the hidden units conditional on those states.
-    error('not yet implemented');
+  % <rbm_w> is a matrix of size <number of hidden units> by <number of visible units>
+  % <visible_state> is a binary matrix of size <number of visible units> by <number of configurations that we're handling in parallel>.
+  % The returned value is a matrix of size <number of hidden units> by <number of configurations that we're handling in parallel>.
+  % This takes in the (binary) states of the visible units, and returns the activation probabilities of the hidden units conditional on those states.
+  m = size(visible_state, 2);
+  number_hidden = size(rbm_w, 1);
+  number_visible = size(visible_state, 1);
+  hidden_probability = zeros(number_hidden, m);
+
+  for k = 1:m
+    visible_state_k = visible_state(:, k);
+    hidden_probability(:, k) = 1 ./ (1 + exp(-(rbm_w * visible_state_k)));
+  end
 end
